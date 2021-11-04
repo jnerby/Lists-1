a7ebc780972fd9f4a141146cd2e1b4bf53ecfae7
Jen Nerby <nerby.jennifer@gmail.com>
2021-11-04 13:02:17 -0600

    inner 4

--- list_operations.py
+++ list_operations.py
@@ -88,7 +88,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -101,7 +101,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
