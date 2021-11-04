ec2136bc24f561bc4782dd149d605bb74629593c
Jen Nerby <nerby.jennifer@gmail.com>
2021-11-04 13:13:06 -0600

    all pass

--- list_operations.py
+++ list_operations.py
@@ -117,7 +117,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -131,7 +131,7 @@ def replace_head(input_list):
     True
 
     """
-
+    input_list[0] = 42
     pass
 
 
@@ -146,7 +146,8 @@ def replace_third_and_last(input_list):
     True
 
     """
-
+    input_list[2] = 37
+    input_list[-1] = 37
     pass
 
 
@@ -165,6 +166,7 @@ def replace_middle(input_list):
     True
 
     """
+    input_list[2:-2] = [42, 37]
 
     pass
 
@@ -180,7 +182,8 @@ def delete_third_and_seventh(input_list):
     True
 
     """
-
+    del input_list[2:7:4]
+    
     pass
 
 
@@ -197,6 +200,8 @@ def delete_middle(input_list):
     True
 
     """
+
+    del input_list[2:-2]
 
     pass
 
